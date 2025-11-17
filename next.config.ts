6fdac7105db8c65f4568335afd0686ed814daf1c
maggiegenova <maggiegenova@gmail.com>
2025-11-17 09:58:21 +0100

    Initial deployment changes

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // enables static export
   // any other config you already have
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/", 
 };
 
 export default nextConfig;
